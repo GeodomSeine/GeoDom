480dce19889737e492b34ee60d7540eb41477fea
ahmed.soussi <soussiahmed62@gmail.com>
2025-02-11 17:34:20 +0100

    map before layers

--- FrontEnd/src/Components/Espace1Component/MapComponent.tsx
+++ FrontEnd/src/Components/Espace1Component/MapComponent.tsx
@@ -26,30 +26,28 @@ interface MapComponentProps {
   setIdHydStart: (id: number | null) => void;
   setIdHydEnd: (id: number | null) => void;
   selectedPk?: GeoJsonResponse;
-  mode : "complet" | "amont-aval";
+  mode: "complet" | "amont-aval";
 }
 
-const CustomControls: React.FC<{ bounds: LatLngBounds | null }> = ({
-  bounds,
-}) => {
+const CustomControls: React.FC<{ bounds: LatLngBounds | null }> = ({ bounds }) => {
   const map = useMap();
   const zoomToBounds = () => {
     bounds && map.fitBounds(bounds);
   };
-  
+
   return (
     <div className="custom_buttons">
-    {bounds && (
-      <LogoComponent Icon={Expand} size={"35px"} onClick={zoomToBounds}/>
-    )}
-    <LogoComponent
+      {bounds && (
+        <LogoComponent Icon={Expand} size={"35px"} onClick={zoomToBounds} />
+      )}
+      <LogoComponent
         Icon={Add}
         size={"35px"}
-        onClick={() => map.setZoom(map.getZoom() + 1)}/>
-    <LogoComponent
+        onClick={() => map.setZoom(map.getZoom() + 1)} />
+      <LogoComponent
         Icon={Minus}
         size={"35px"}
-        onClick={() => map.setZoom(map.getZoom() + (-1))}/>
+        onClick={() => map.setZoom(map.getZoom() - 1)} />
     </div>
   );
 };
@@ -62,10 +60,9 @@ const MapComponent: React.FC<MapComponentProps> = ({
   amontAvalResponse,
   setIdHydStart,
   setIdHydEnd,
-  selectedPk, 
+  selectedPk,
   mode
 }) => {
-  console.log("exutoire_id: ", exutoire_id);
   const [hydroData, setHydroData] = useState<GeoJsonResponse | null>(null);
   const [bassinData, setBassinData] = useState<GeoJsonResponse | null>(null);
   const [hydroStyles, setHydroStyles] = useState<any[]>([]);
@@ -74,18 +71,15 @@ const MapComponent: React.FC<MapComponentProps> = ({
   const [stationSnapStyles, setStationSnapStyles] = useState<PathOptions | null>(null);
   const idHydStartRef = useRef<number | null>(idHydStart);
   const idHydEndRef = useRef<number | null>(idHydEnd);
-  const [loading, setLoading] = useState(true);
   const [bounds, setBounds] = useState<LatLngBounds | null>(null);
-  
+
   useEffect(() => {
     idHydStartRef.current = idHydStart;
     idHydEndRef.current = idHydEnd;
   }, [idHydStart, idHydEnd]);
 
-
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true);
       try {
         // Exécuter toutes les requêtes en parallèle
         const [
@@ -103,18 +97,18 @@ const MapComponent: React.FC<MapComponentProps> = ({
           getHydro(program),
           getStationSnap(program)
         ]);
-  
+
         // Traitement des données obtenues
         if (hydroSLDData) {
           const hydroSLDText = await hydroSLDData.text();
           setHydroStyles(parseSLDToStyles(hydroSLDText));
         }
-  
+
         if (bassinData) {
           setBassinData(bassinData);
           setBounds(calculateBounds(bassinData));
         }
-  
+
         if (bassinSLDData) {
           const bassinSLDText = await bassinSLDData.text();
           const styles = parseSLDToStyles(bassinSLDText);
@@ -123,7 +117,7 @@ const MapComponent: React.FC<MapComponentProps> = ({
             weight: styles[0]?.weight || 3,
           });
         }
-  
+
         if (stationSLDData) {
           const stationSLDText = await stationSLDData.text();
           const styles = parseSLDToStyles(stationSLDText);
@@ -132,111 +126,107 @@ const MapComponent: React.FC<MapComponentProps> = ({
             weight: styles[0]?.weight || 3,
           });
         }
-  
+
         if (hydroData) {
           setHydroData(hydroData);
         }
-  
+
         if (stationData) {
           setStationSnap(stationData);
         }
-  
+
       } catch (error) {
         console.error("Erreur lors du chargement des données :", error);
-      } finally {
-        setLoading(false);
       }
     };
-  
+
     fetchData();
   }, [program]);
 
-  
   const calculateBounds = (bassin: GeoJsonResponse): LatLngBounds => {
     const coordinates = bassin.features[0]?.geometry?.coordinates[0];
     if (!coordinates) return new LatLngBounds([0, 0], [0, 0]);
-    
+
     let minLat = 90,
-    maxLat = -90,
-    minLng = 180,
-    maxLng = -180;
-    
+      maxLat = -90,
+      minLng = 180,
+      maxLng = -180;
+
     coordinates.forEach(([lng, lat]: number[]) => {
       if (lat < minLat) minLat = lat;
       if (lat > maxLat) maxLat = lat;
       if (lng < minLng) minLng = lng;
       if (lng > maxLng) maxLng = lng;
-    });
-    
-    return new LatLngBounds([minLat, minLng], [maxLat, maxLng]);
-  };
-  
-  const getHydroStyle = (feature: any): PathOptions => {
-    const strahler = feature.properties?.strahler;
-    const id = feature.properties?.id_hyd;
-    
-    if (amontAvalResponse?.id_hyd.includes(id))
-      return { color: "var(--warning-color)", weight: 3 };
-    if (idHydStart === id || idHydEnd === id)
-      return { color: "var(--success-color)", weight: 4 };
-    
-    for (const rule of hydroStyles) {
-      if (strahler >= rule.min && strahler <= rule.max)
-        return { color: rule.color, weight: rule.weight };
-    }
-    
-    return { color: "var(--basic-black)", weight: 1 };
-  };
-  
-  const handleFeatureClick = (feature: { properties: { [key: string]: any } }, layer: any) => {
-    const properties = feature.properties;
-  
-    const popupContent = document.createElement("div");
-    popupContent.setAttribute("class", "leaflet-elements-container")
-  
-    const onSelectAmont = () => {
-      setIdHydStart(properties.id_hyd);
-      setIdHydEnd(exutoire_id);
-      layer.closePopup();
-    };
-  
-    const onSelectAval = () => {
-      setIdHydEnd(properties.id_hyd);
-      layer.closePopup();
-    };
-  
-    const root = createRoot(popupContent);
-    root.render(
-      <PopupContent
-        properties={properties}
-        mode={mode}
-        onSelectAmont={onSelectAmont}
-        onSelectAval={onSelectAval}
-      />
-    );
-  
-    layer.bindPopup(popupContent).openPopup();
-  };    
-  
-  return (
-    <div className="map_component">
-    {!loading && bounds && (
-      <MapContainer
-        attributionControl={false}
-        bounds={bounds}
-        zoom={7}
-        minZoom={7}
-        zoomControl={false}
-      >
-      <CustomControls bounds={bounds} />
-      <LayersControl>
-      <BaseLayer checked name="baseLayer">
+});
+
+return new LatLngBounds([minLat, minLng], [maxLat, maxLng]);
+};
+
+const getHydroStyle = (feature: any): PathOptions => {
+const strahler = feature.properties?.strahler;
+const id = feature.properties?.id_hyd;
+
+if (amontAvalResponse?.id_hyd.includes(id))
+  return { color: "var(--warning-color)", weight: 3 };
+if (idHydStart === id || idHydEnd === id)
+  return { color: "var(--success-color)", weight: 4 };
+
+for (const rule of hydroStyles) {
+  if (strahler >= rule.min && strahler <= rule.max)
+    return { color: rule.color, weight: rule.weight };
+}
+
+return { color: "var(--basic-black)", weight: 1 };
+};
+
+const handleFeatureClick = (feature: { properties: { [key: string]: any } }, layer: any) => {
+const properties = feature.properties;
+
+const popupContent = document.createElement("div");
+popupContent.setAttribute("class", "leaflet-elements-container");
+
+const onSelectAmont = () => {
+  setIdHydStart(properties.id_hyd);
+  setIdHydEnd(exutoire_id);
+  layer.closePopup();
+};
+
+const onSelectAval = () => {
+  setIdHydEnd(properties.id_hyd);
+  layer.closePopup();
+};
+
+const root = createRoot(popupContent);
+root.render(
+  <PopupContent
+    properties={properties}
+    mode={mode}
+    onSelectAmont={onSelectAmont}
+    onSelectAval={onSelectAval}
+  />
+);
+
+layer.bindPopup(popupContent).openPopup();
+};
+
+return (
+<div className="map_component">
+  <MapContainer
+      attributionControl={false}
+      bounds={bounds || [[50.9, -1.5], [46.5, 8.5]]}
+      zoom={6} 
+      minZoom={6}
+      zoomControl={false}
+  >
+    <CustomControls bounds={bounds} />
+    <LayersControl>
+      <BaseLayer checked name="BaseLayer">
         <TileLayer
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution='&copy; <a href="https://carto.com/">CARTO</a>'
         />
       </BaseLayer>
-      
+
       {stationSnap && stationSnapStyles && (
         <Overlay name="Stations d'observation">
           <GeoJSON
@@ -244,13 +234,14 @@ const MapComponent: React.FC<MapComponentProps> = ({
             pointToLayer={(_, latlng) => {
               const style = {
                 ...stationSnapStyles,
-                radius: 5, 
+                radius: 5,
               };
               return new CircleMarker(latlng, style);
             }}
           />
         </Overlay>
       )}
+
       {hydroData && hydroStyles && (
         <Overlay checked name="Hydrographie">
           <GeoJSON
@@ -267,6 +258,7 @@ const MapComponent: React.FC<MapComponentProps> = ({
           />
         </Overlay>
       )}
+
       {bassinData && bassinStyle && (
         <Overlay checked name="Bassin">
           <GeoJSON
@@ -276,15 +268,21 @@ const MapComponent: React.FC<MapComponentProps> = ({
           />
         </Overlay>
       )}
+
       {selectedPk && (
         <Overlay checked name="PK">
-          <GeoJSON key={JSON.stringify(selectedPk)} data={selectedPk as GeoJsonObject} style={{ color: 'var(--success-color)', weight: 6 }} interactive={false} />
+          <GeoJSON
+            key={JSON.stringify(selectedPk)}
+            data={selectedPk as GeoJsonObject}
+            style={{ color: "var(--success-color)", weight: 6 }}
+            interactive={false}
+          />
         </Overlay>
       )}
-      </LayersControl>
-      </MapContainer>
-    )}
-    </div>
-  );
+    </LayersControl>
+  </MapContainer>
+</div>
+);
 };
+
 export default MapComponent;
