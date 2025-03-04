4171ea7993906e7c5323e1806e6409b262e07a69
Gorgio-Bellounti <panizzi.brice@gmail.com>
2025-03-04 16:02:57 +0100

    change csv export

--- FrontEnd/src/Components/ExportComponent/ExportCsvComponent.tsx
+++ FrontEnd/src/Components/ExportComponent/ExportCsvComponent.tsx
@@ -1,8 +1,10 @@
 import ButtonComponent from '../SimpleComponents/ButtonComponent';
 import React from 'react';
 import Papa from 'papaparse';
+import './ExportComponent.scss';
 import { DataResponse, DonutsDataResponse, Scenario } from '../../services/api';
 import { transformData } from '../../utils/dataTransform';
+import JSZip from 'jszip';
 
 interface ExportCsvComponentProps {
     exportCsvData: {
@@ -10,17 +12,33 @@ interface ExportCsvComponentProps {
         mode: string;
         pynutsData: DataResponse | null;
         donutsData: DonutsDataResponse | null;
+        variables: string[]; 
         scenarios: Scenario[]
     }
 }
 
 const ExportCsvComponent: React.FC<ExportCsvComponentProps> = ({ exportCsvData }) => {
-    const handleExport = () => {
+    const handleExport = async () => {
         if (!exportCsvData.pynutsData || !exportCsvData.donutsData) return;
 
-        const transformedData = transformData(exportCsvData.pynutsData, exportCsvData.donutsData, exportCsvData.scenarios, exportCsvData.mode);
+        const zip = new JSZip();
 
+        const transformedData = transformData(exportCsvData.pynutsData, exportCsvData.donutsData, exportCsvData.scenarios, exportCsvData.mode);
         const csv = Papa.unparse(transformedData);
+
+        zip.file(`${exportCsvData.name}.csv`, csv);
+        
+        const metadata = {
+            name: exportCsvData.name,
+            date: new Date().toISOString(),
+            variables: exportCsvData.variables,
+            annees: exportCsvData.scenarios.map((s) => s.year),
+        };
+
+        zip.file("metadata.json", JSON.stringify(metadata, null, 2));       
+
+        const zipBlob = await zip.generateAsync({ type: "blob" });
+        
         const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
         const url = URL.createObjectURL(blob);
         const link = document.createElement('a');
@@ -32,11 +50,15 @@ const ExportCsvComponent: React.FC<ExportCsvComponentProps> = ({ exportCsvData }
     }
 
     return (
-        <ButtonComponent
-            onClick={handleExport}
-            txt='CSV'
-            className='button_container'
-        />
+        <div className="export_container CSV">
+            <div className='export_body'>
+                <ButtonComponent
+                    onClick={handleExport}
+                    txt='CSV'
+                    className='button_container'
+                />
+            </div>
+        </div>
     );
 }
 
